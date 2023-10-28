317de81247efd329c71599a8d96b1d0673c43ff4
shweyeewinaung <shweyeewinaung@gmail.com>
2023-10-28 16:00:49 +0700

    updated head tag info

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <Head>
+      <Title>Task Master by Shwe</Title>
+      <Meta name="description" content="This is Shwe's Task Master application created for Friz" />
+    </Head>
   <NuxtLayout>
     <NuxtPage/>
   </NuxtLayout>
